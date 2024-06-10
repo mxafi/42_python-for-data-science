3eb3d79aaa2183a26f58d504a0940b8c907dadd4
Markus Laaksonen <markus@mxa.fi>
2024-06-10 19:56:19 +0300

    feat(d0ex08): naive implementation

--- d0/ex08/Loading.py
+++ d0/ex08/Loading.py
@@ -1,2 +1,10 @@
 def ft_tqdm(lst: range) -> None:
-    pass
+    total = len(lst)
+    bar_length = 80
+    for i, item in enumerate(lst):
+        yield item
+        frac = (i+1) / total
+        arrow = int(frac * bar_length - 1) * '=' + '>'
+        padding = int(bar_length - len(arrow)) * ' '
+        en = '\n' if i+1 == total else '\r'
+        print(f"{int(frac*100)}%|[{arrow}{padding}]| {i+1}/{total}", end=en)
