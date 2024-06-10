33e1be28500555beaed45cbbe2174c8d88290628
Markus Laaksonen <markus@mxa.fi>
2024-06-10 12:44:50 +0300

    feat(d0ex04): make whatis.py worse to comply
    subject implies you have to throw an actual AssertionError, just printing the text is not ok

--- d0/ex04/whatis.py
+++ d0/ex04/whatis.py
@@ -1,18 +1,28 @@
 import sys as s
 
+def isInt(i: str) -> bool:
+    try:
+        int(i)
+        return True
+    except ValueError:
+        return False
+
 def whatis():
     if len(s.argv) == 1:
         return
-    if len(s.argv) > 2:
-        print("AssertionError: more than one argument is provided")
+    try:
+        assert len(s.argv) < 3, "more than one argument is provided"
+    except AssertionError as e:
+        print(f"{e.__class__.__name__}: {e}")
         return
 
     try:
-        input = int(s.argv[1])
-    except:
-        print("AssertionError: argument is not an integer")
+        assert isInt(s.argv[1]), "argument is not an integer"
+    except AssertionError as e:
+        print(f"{e.__class__.__name__}: {e}")
         return
 
+    input = int(s.argv[1])
     if input % 2 == 0:
         print("I'm Even.")
     else:
