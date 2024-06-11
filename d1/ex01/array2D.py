3902ab053e12a07007b200c6c1dc49ab1adcb05c
Markus Laaksonen <markus@mxa.fi>
2024-06-11 13:25:24 +0300

    feat(d1ex01): init

---
+++ d1/ex01/array2D.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    pass
+
+
+def main():
+    """Testmain for slice_me"""
+
+
+if __name__ == "__main__":
+    main()
---
+++ d1/ex01/tester.py
@@ -0,0 +1,17 @@
+from array2D import slice_me
+
+# $> python test_array2D.py
+# My shape is : (4, 2)
+# My new shape is : (2, 2)
+# [[1.8, 78.4], [2.15, 102.7]]
+# My shape is : (4, 2)
+# My new shape is : (1, 2)
+# [[2.15, 102.7]]
+# $>
+
+family = [[1.80, 78.4],
+          [2.15, 102.7],
+          [2.10, 98.5],
+          [1.88, 75.2]]
+print(slice_me(family, 0, 2))
+print(slice_me(family, 1, -2))
