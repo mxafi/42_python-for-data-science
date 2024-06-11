aa2a5372a4e469f71751a18632fd3441772390be
Markus Laaksonen <markus@mxa.fi>
2024-06-11 08:52:23 +0300

    feat(d1ex00): init

---
+++ d1/ex00/give_bmi.py
@@ -0,0 +1,14 @@
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    pass
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
