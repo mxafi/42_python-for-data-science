3031d79601d835409b0ed64c4ecbd924a780119f
Markus Laaksonen <markus@mxa.fi>
2024-06-11 14:10:36 +0300

    feat(d1ex02): init

---
+++ d1/ex02/load_image.py
@@ -0,0 +1,16 @@
+
+def ft_load(path: str) -> array:
+    """
+    Loads an image in JPG or JPEG format,
+    prints its format and its pixels' content in RGB format
+    """
+    pass
+
+
+def main():
+    """Testmain for ft_load"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ d1/ex02/tester.py
@@ -0,0 +1,14 @@
+# Expected output:
+# $> python tester.py
+# The shape of image is: (257, 450, 3)
+# [[[19 42 83]
+# [23 42 84]
+# [28 43 84]
+# ...
+# [ 0 0 0]
+# [ 1 1 1]
+# [ 1 1 1]]]
+# $>
+
+from load_image import ft_load
+print(ft_load("landscape.jpg"))
