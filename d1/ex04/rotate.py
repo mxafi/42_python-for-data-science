de28275c29956a6f29fe93f315e5f84191ca7428
Markus Laaksonen <markus@mxa.fi>
2024-06-11 18:26:00 +0300

    feat(d1ex04): init

---
+++ d1/ex04/rotate.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+import numpy as np
+from load_image import ft_load
+
+
+def main():
+    """Program loads an image "animal.jpeg",
+    prints info:
+    - Size in pixels on X and Y axis
+    - Number of channels
+    - Pixel content of the image
+    "zooms" the image,
+    displays the image after zooming
+    """
+    a = ft_load("animal.jpeg")
+    if a.size == 1 and a[0] == "error_load_image":
+        return
+
+    im = Image.fromarray(a)
+    width, height = im.size
+    left = int(0.44 * width)
+    top = int(0.12 * height)
+    right = min(int(left + 400), width)
+    bottom = min(int(top + 400), height)
+    im = im.crop((left, top, right, bottom))
+    im = ImageOps.grayscale(im)
+    a2 = np.array(im)
+    a2 = a2.reshape(a2.shape[0], a2.shape[1], 1)
+    print("The shape of image is:", a2.shape)
+    print(a2)
+
+    # transpose here
+
+    plt.imshow(im, cmap='gray')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
