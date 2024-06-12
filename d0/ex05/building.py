f5c5b78bf6b2c8a757356ebe5b5919e2888dceb1
Markus Laaksonen <markus@mxa.fi>
2024-06-12 18:52:18 +0300

    fix(d0ex05): handle empty input
    check for empty input and prevent line 28 from crashing due to reading index -1 from empty string

--- d0/ex05/building.py
+++ d0/ex05/building.py
@@ -14,32 +14,34 @@ def main():
     argc = len(s.argv)
     try:
         assert argc <= 2, "too many arguments"
+        if argc == 1 or (argc == 2 and s.argv[1] == ""):
+            try:
+                print("What is the text to count?")
+                input_string = s.stdin.readline()
+            except EOFError:
+                raise AssertionError("received EOF, no input?")
+        else:
+            input_string = s.argv[1]
+        input_len = len(input_string)
+        assert input_len > 0, "empty input"
+        assert input_len <= 1000000, "too long input"
+        if input_string[-1] != "\n":
+            print("")
+        print(f"The text contains {input_len} characters:")
+        count_upper = sum(1 for c in input_string if c.isupper())
+        count_lower = sum(1 for c in input_string if c.islower())
+        punct = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        count_punct = sum(1 for c in input_string if c in punct)
+        count_space = sum(1 for c in input_string if c.isspace())
+        count_digit = sum(1 for c in input_string if c.isdigit())
+        print(f"{count_upper} upper letters")
+        print(f"{count_lower} lower letters")
+        print(f"{count_punct} punctuation marks")
+        print(f"{count_space} spaces")
+        print(f"{count_digit} digits")
     except AssertionError as e:
         print(f"{e.__class__.__name__}: {e}")
         exit(1)
-    if argc == 1 or (argc == 2 and s.argv[1] == ""):
-        try:
-            print("What is the text to count?")
-            input_string = s.stdin.readline()
-        except EOFError:
-            input_string = ""
-    else:
-        input_string = s.argv[1]
-    input_len = len(input_string)
-    if input_string[-1] != "\n":
-        print("")
-    print(f"The text contains {input_len} characters:")
-    count_upper = sum(1 for c in input_string if c.isupper())
-    count_lower = sum(1 for c in input_string if c.islower())
-    punct = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    count_punct = sum(1 for c in input_string if c in punct)
-    count_space = sum(1 for c in input_string if c.isspace())
-    count_digit = sum(1 for c in input_string if c.isdigit())
-    print(f"{count_upper} upper letters")
-    print(f"{count_lower} lower letters")
-    print(f"{count_punct} punctuation marks")
-    print(f"{count_space} spaces")
-    print(f"{count_digit} digits")
 
 
 if __name__ == "__main__":
