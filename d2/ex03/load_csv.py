041ffe01509fe81f5d04ad2b4ea70e734b845e1f
Markus Laaksonen <markus@mxa.fi>
2024-06-12 18:39:47 +0300

    feat(d2ex03): init

---
+++ d2/ex03/load_csv.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame | None:
+    """
+    Load a CSV file into a pandas DataFrame.
+
+    Parameters:
+    path (str): The path to the CSV file.
+
+    Returns:
+    pd.DataFrame | None: The loaded DataFrame if successful, None otherwise.
+    """
+    try:
+        assert path.endswith(".csv"), "Invalid file format (not CSV)."
+        df = pd.read_csv(path)
+        print("Loading dataset of dimensions", df.shape)
+        return df
+    except (AssertionError,
+            FileNotFoundError,
+            IsADirectoryError,
+            PermissionError
+            ) as e:
+        print(f"{e.__class__.__name__}: {e}")
+        return None
+
+
+def main():
+    """
+    This is the test main function of the script.
+    It loads the 'life_expectancy_years.csv' file and prints the result.
+    """
+    print(load("life_expectancy_years.csv"))
+
+
+if __name__ == "__main__":
+    main()
