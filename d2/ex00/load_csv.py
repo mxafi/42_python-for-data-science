d30983e9b647e7208d0a6ac0c8010a5bfe664c59
Markus Laaksonen <markus@mxa.fi>
2024-06-12 09:39:41 +0300

    feat(d2ex00): implement load function

--- d2/ex00/load_csv.py
+++ d2/ex00/load_csv.py
@@ -1,12 +1,34 @@
 import pandas as pd
 
 
-def load(path: str) -> Dataset:
-    pass
+def load(path: str) -> pd.DataFrame | None:
+    """
+    Load a CSV file into a pandas DataFrame.
+
+    Parameters:
+    path (str): The path to the CSV file.
+
+    Returns:
+    pd.DataFrame | None: The loaded DataFrame if successful, None otherwise.
+    """
+    try:
+        df = pd.read_csv(path)
+        print("Loading dataset of dimensions", df.shape)
+        return df
+    except (FileNotFoundError,
+            IsADirectoryError,
+            PermissionError
+            ) as e:
+        print(f"{e.__class__.__name__}: {e}")
+        return None
 
 
 def main():
-    pass
+    """
+    This is the test main function of the script.
+    It loads the 'life_expectancy_years.csv' file and prints the result.
+    """
+    print(load("life_expectancy_years.csv"))
 
 
 if __name__ == "__main__":
