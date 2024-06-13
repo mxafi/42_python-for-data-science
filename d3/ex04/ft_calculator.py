100014a44bd2f3c14c30ea5f7ca35cddc64d4798
Markus Laaksonen <markus@mxa.fi>
2024-06-13 13:45:32 +0300

    feat(d3ex04): implement

--- d3/ex04/ft_calculator.py
+++ d3/ex04/ft_calculator.py
@@ -1,12 +1,18 @@
 class calculator:
-    # decorator
-    def dotproduct(self, V1: list[float], V2: list[float]) -> None:
-        pass
+    """This class can perform some operations on vectors."""
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """This method calculates the dot product of two vectors."""
+        print("Dot product is: ", sum(x * y for x, y in zip(V1, V2)))
 
-    # decorator
-    def add_vec(self, V1: list[float], V2: list[float]) -> None:
-        pass
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        """This method adds two vectors."""
+        res = [float(x + y) for x, y in zip(V1, V2)]
+        print(f"Add Vector is : {res}")
 
-    # decorator
-    def sous_vec(self, V1: list[float], V2: list[float]) -> None:
-        pass
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """This method subtracts two vectors."""
+        res = [float(x - y) for x, y in zip(V1, V2)]
+        print(f"Sous Vector is: {res}")
