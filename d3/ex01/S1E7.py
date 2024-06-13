bc76df0bb497ba1ec819b209670c406d5c75f9d5
Markus Laaksonen <markus@mxa.fi>
2024-06-13 12:20:58 +0300

    feat(d3ex01): implement str and repr dunders

--- d3/ex01/S1E7.py
+++ d3/ex01/S1E7.py
@@ -13,6 +13,14 @@ class Baratheon(Character):
     def create_baratheon(cls, first_name, is_alive=True):
         return cls(first_name, is_alive)
 
+    def __str__(self):
+        """Return a string representation of the character."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """Return a string representation of the character."""
+        return self.__str__()
+
 
 class Lannister(Character):
     """Representing the Lannister family."""
@@ -25,3 +33,11 @@ class Lannister(Character):
     @classmethod
     def create_lannister(cls, first_name, is_alive=True):
         return cls(first_name, is_alive)
+
+    def __str__(self):
+        """Return a string representation of the character."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """Return a string representation of the character."""
+        return self.__str__()
