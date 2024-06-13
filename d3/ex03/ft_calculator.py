85ad1262c55b2804777b59c9d1f8ef646f554a1e
Markus Laaksonen <markus@mxa.fi>
2024-06-13 13:23:37 +0300

    feat(d3ex03): implement calculator

--- d3/ex03/ft_calculator.py
+++ d3/ex03/ft_calculator.py
@@ -1,15 +1,32 @@
 class calculator:
-    def __init__(self) -> None:
-        pass
+    """A class to perform basic operations on a list of numbers."""
+    def __init__(self, vec) -> None:
+        """Initialize the class with a list of numbers."""
+        self.vec = vec
 
     def __add__(self, object) -> None:
-        pass
+        """Add a number to each element of the list."""
+        res = [x + object for x in self.vec]
+        print(res)
+        self.vec = res
 
     def __mul__(self, object) -> None:
-        pass
+        """Multiply each element of the list by a number."""
+        res = [x * object for x in self.vec]
+        print(res)
+        self.vec = res
 
     def __sub__(self, object) -> None:
-        pass
+        """Subtract a number from each element of the list."""
+        res = [x - object for x in self.vec]
+        print(res)
+        self.vec = res
 
     def __truediv__(self, object) -> None:
-        pass
+        """Divide each element of the list by a number."""
+        if object == 0:
+            print("Error: Division by zero is not possible")
+            return
+        res = [x / object for x in self.vec]
+        print(res)
+        self.vec = res
