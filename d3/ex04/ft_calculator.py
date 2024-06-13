17b92c77bd298d3fb6ae332cf07d7f46c4b44db0
Markus Laaksonen <markus@mxa.fi>
2024-06-13 13:29:12 +0300

    feat(d3ex04): init

---
+++ d3/ex04/ft_calculator.py
@@ -0,0 +1,12 @@
+class calculator:
+    # decorator
+    def dotproduct(self, V1: list[float], V2: list[float]) -> None:
+        pass
+
+    # decorator
+    def add_vec(self, V1: list[float], V2: list[float]) -> None:
+        pass
+
+    # decorator
+    def sous_vec(self, V1: list[float], V2: list[float]) -> None:
+        pass
