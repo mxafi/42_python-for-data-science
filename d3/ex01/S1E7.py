2510c5266eab80e6557e786c885e24d97cbaff87
Markus Laaksonen <markus@mxa.fi>
2024-06-13 11:47:08 +0300

    feat(d3ex01): init

---
+++ d3/ex01/S1E7.py
@@ -0,0 +1,9 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    pass
+
+
+class Lannister(Character):
+    pass
---
+++ d3/ex01/S1E9.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """An abstract base class representing a character."""
+    @abstractmethod
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor for Character class."""
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+        """Kill the character from the Character class."""
+        self.is_alive = False
+
+
+class Stark(Character):
+    """A derived class Stark inheriting from abstract Character."""
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Constructor for Stark class."""
+        super().__init__(first_name, is_alive)
+
+    def die(self):
+        """Kill the character from the Stark class."""
+        super().die()
