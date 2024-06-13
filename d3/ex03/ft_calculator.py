e5711b2b08d5cec4aa972c968642c2aa254abc65
Markus Laaksonen <markus@mxa.fi>
2024-06-13 13:09:11 +0300

    feat(d3ex03): init

---
+++ d3/ex03/ft_calculator.py
@@ -0,0 +1,15 @@
+class calculator:
+    def __init__(self) -> None:
+        pass
+
+    def __add__(self, object) -> None:
+        pass
+
+    def __mul__(self, object) -> None:
+        pass
+
+    def __sub__(self, object) -> None:
+        pass
+
+    def __truediv__(self, object) -> None:
+        pass
