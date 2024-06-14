06592e49f753d10ddc8e8e9c7563882380cc120c
Markus Laaksonen <markus@mxa.fi>
2024-06-14 11:56:10 +0300

    feat(d4ex01): implement

--- d4/ex01/in_out.py
+++ d4/ex01/in_out.py
@@ -1,10 +1,32 @@
 def square(x: int | float) -> int | float:
-    pass
+    """Return the square of x."""
+    if isinstance(x, int | float):
+        return x * x
+    else:
+        raise ValueError("x must be an int or a float.")
+
 
 def pow(x: int | float) -> int | float:
-    pass
+    """Return x to the power of x."""
+    if isinstance(x, int | float):
+        return x ** x
+    else:
+        raise ValueError("x must be an int or a float.")
+
 
 def outer(x: int | float, function) -> object:
+    """Return a function that applies the given function to x
+    the number of times it's called.
+    """
     count = 0
+
     def inner() -> float:
-        pass
+        """Return the result of applying the function to x, count times."""
+
+        nonlocal count
+        res = function(x)
+        for _ in range(count):
+            res = function(res)
+        count += 1
+        return res
+    return inner
