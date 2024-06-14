bf34b5e2969799e9c4c92f4c5bf68814a422c5f3
Markus Laaksonen <markus@mxa.fi>
2024-06-14 12:07:07 +0300

    feat(d4ex02): init

---
+++ d4/ex02/tester.py
@@ -0,0 +1,23 @@
+from callLimit import callLimit
+
+# $> python tester.py
+# f()
+# g()
+# f()
+# Error: <function g at 0x7fabdc243ee0> call too many times
+# f()
+# Error: <function g at 0x7fabdc243ee0> call too many times
+# $>
+
+@callLimit(3)
+def f():
+    print("f()")
+
+@callLimit(1)
+def g():
+    print("g()")
+
+
+for i in range(3):
+    f()
+    g()
