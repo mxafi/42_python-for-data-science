5ead0a29e4872565439b057316715b89564fec5f
Markus Laaksonen <markus@mxa.fi>
2024-06-14 11:29:18 +0300

    feat(d4ex01): init

---
+++ d4/ex01/in_out.py
@@ -0,0 +1,10 @@
+def square(x: int | float) -> int | float:
+    pass
+
+def pow(x: int | float) -> int | float:
+    pass
+
+def outer(x: int | float, function) -> object:
+    count = 0
+    def inner() -> float:
+        pass
