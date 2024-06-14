27ce9b39f6cbec1703b1f1e8e23960700c426e31
Markus Laaksonen <markus@mxa.fi>
2024-06-14 08:58:18 +0300

    feat(d4ex00): structure

--- d4/ex00/statistics.py
+++ d4/ex00/statistics.py
@@ -1,2 +1,40 @@
+def check_numbers(numbers: list) -> bool:
+    if not numbers:
+        return False
+    for number in numbers:
+        if not isinstance(number, (int, float)):
+            return False
+    return True
+
+
 def ft_statistics(*args, **kwargs) -> None:
-    pass
+    numbers = list(args)
+    isvalid = check_numbers(numbers)
+    for value in kwargs:
+        if value == 'mean':
+            if isvalid:
+                pass
+            else:
+                print('ERROR')
+        elif value == 'median':
+            if isvalid:
+                pass
+            else:
+                print('ERROR')
+        elif value == 'quartile':
+            if isvalid:
+                pass
+            else:
+                print('ERROR')
+        elif value == 'std':
+            if isvalid:
+                pass
+            else:
+                print('ERROR')
+        elif value == 'var':
+            if isvalid:
+                pass
+            else:
+                print('ERROR')
+        else:
+            pass
